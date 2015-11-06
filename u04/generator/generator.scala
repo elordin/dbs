55f850330da06f57f4ec1e062f77aa29e73955bc
Thomas Weber <weber@openinnovator.net>
2015-11-06 17:21:18 +0100

    more deterministic (but too deterministic) behavior of gen.

--- u04/generator/generator.scala
+++ u04/generator/generator.scala
@@ -16,12 +16,21 @@ object Generator {
     val cdu = Party("CDU", "CDU", "", "black", false)
     val spd = Party("SPD", "SPD", "", "red", false)
 
-    case class Candidacy(val idno:String, val wkid:Int, val year:Int, val supporter:Option[Party]) {}
+
+
+    case class Candidacy(val idno:String, val wkid:Int, val year:Int, val supporter:Option[Party]) {
+        override def toString(): String = idno
+    }
 
     val c1 = Candidacy("abc123", 0, 2013, None)
     val c2 = Candidacy("defghi", 0, 2013, None)
 
+
+
     case class Stimmzettel(gender:Gender, age:Int, erststimme:Candidacy, zweitstimme:Party) {
+        def this(erststimme:Candidacy, zweitstimme:Party) =
+            this(if (Random.nextInt(2) == 1) Male else Female, Random.nextInt(80) + 18, erststimme, zweitstimme)
+
         override def toString(): String =
             f"((${erststimme.idno}, ${erststimme.wkid}, ${erststimme.year}), ${zweitstimme.name}, ${gender}, ${age})"
     }
@@ -60,57 +69,74 @@ object Generator {
             }
 
             val (erststimmenCount, zweitstimmenCount) = votes.foldLeft(Map[Candidacy, Int](), Map[Party, Int]())(cummulate)
-            val totalErststimme = erststimmenCount.values.sum
-            val totalZweistimme = zweitstimmenCount.values.sum
+            val totalErststimme:Double = erststimmenCount.values.sum
+            val totalZweistimme:Double = zweitstimmenCount.values.sum
 
-            new Distribution(erststimmenCount.mapValues(_ / totalErststimme), zweitstimmenCount.mapValues(_ / totalZweistimme))
+            new Distribution(erststimmenCount.mapValues(_ / totalErststimme:Double), zweitstimmenCount.mapValues(_ / totalZweistimme:Double))
         }
 
         override def toString(): String = votes.toString()
-
     }
 
     class Distribution(val erststimmen:Map[Candidacy, Double], val zweitstimmen:Map[Party, Double]) {
         /** Mean deviation from percentages of all values */
         def distance(other:Distribution):Double = {
-            var dist = 0
+            val (erststimmenDiffs, zweitstimmenDiffs) = deviation(other)
+            (erststimmenDiffs.values.map(scala.math.abs(_)).sum /
+                    (erststimmenDiffs.values.toList.length:Double) +
+            zweitstimmenDiffs.values.map(scala.math.abs(_)).sum /
+                (zweitstimmenDiffs.values.toList.length:Double)
+            ) / (2:Double)
+        }
 
+        /** Relative differences between this and others percentages per entry */
+        def deviation(other:Distribution):(Map[Candidacy, Double], Map[Party, Double]) = {
             val allCandidacies:List[Candidacy] = (erststimmen.keys ++ other.erststimmen.keys).toList
             val allParties:List[Party] = (zweitstimmen.keys ++ other.zweitstimmen.keys).toList
 
-            val erststimmenDiffs:List[Double] = allCandidacies.map((c:Candidacy) => {
-                scala.math.abs(erststimmen.getOrElse(c, 0:Double) - other.erststimmen.getOrElse(c, 0:Double))
-            })
+            val erststimmenDiffs:Map[Candidacy, Double] = allCandidacies.map((c:Candidacy) => {
+                (c -> (other.erststimmen.getOrElse(c, 0:Double) - erststimmen.getOrElse(c, 0:Double)))
+            }).toMap
 
-            val zweitstimmenDiffs:List[Double] = allParties.map((p:Party) => {
-                scala.math.abs(zweitstimmen.getOrElse(p, 0:Double) - other.zweitstimmen.getOrElse(p, 0:Double))
-            })
+            val zweitstimmenDiffs:Map[Party, Double] = allParties.map((p:Party) => {
+                (p -> (other.zweitstimmen.getOrElse(p, 0:Double) - zweitstimmen.getOrElse(p, 0:Double)))
+            }).toMap
 
-            (erststimmenDiffs.sum / erststimmenDiffs.length + zweitstimmenDiffs.sum / zweitstimmenDiffs.length) / 2
+
+            println("> " + erststimmenDiffs)
+
+            // (erststimmenDiffs, zweitstimmenDiffs)
+
+            (Map((c1 -> 0.5), (c2 -> -0.5)), Map((cdu -> -0.5), (spd -> 0.5)))
+        }
+
+        def inNeedOfErststimme(other:Distribution):Candidacy = {
+            val folksWithVotesMissing:List[Candidacy] = deviation(other)._1.filter(_._2 < 0).map(_._1).toList
+            if (folksWithVotesMissing.length < 1)
+                Stimmzettel.possibleCandidates.apply(Random.nextInt(Stimmzettel.possibleCandidates.length))
+            else
+                folksWithVotesMissing.apply(Random.nextInt(folksWithVotesMissing.length))
+        }
+
+        def inNeedOfZweitstimme(other:Distribution):Party = {
+            val folksWithVotesMissing:List[Party] = deviation(other)._2.filter(_._2 < 0).map(_._1).toList
+            if (folksWithVotesMissing.length < 1)
+                Stimmzettel.possibleParties.apply(Random.nextInt(Stimmzettel.possibleParties.length))
+            else
+                folksWithVotesMissing.apply(Random.nextInt(folksWithVotesMissing.length))
         }
 
         override def toString(): String = f"${erststimmen}\n${zweitstimmen}"
-
     }
+
 
     def generate(targetDist:Distribution, size:Int):Ergebnis = {
         var result:Ergebnis = new Ergebnis(Set[Stimmzettel]())
         for (i <- 1 to size) {
-            var epsilon = (size - i + 0.1) * (size - i + 0.1)
-            var newvote:Stimmzettel = Stimmzettel.random
-            var potentialResult:Ergebnis = result.insert(newvote)
-            var newDistributionDistance:Double = result.distribution.distance(targetDist)
-            var c = 0
-
-            while (newDistributionDistance > epsilon && c < 10000) {
-                c += 1
-                newvote = Stimmzettel.random
-                potentialResult = result.insert(newvote)
-                newDistributionDistance = result.distribution.distance(targetDist)
-            }
-            assert(c < 9999)
-
-            result = potentialResult
+            var newvote:Stimmzettel = new Stimmzettel(
+                result.distribution.inNeedOfErststimme(targetDist),
+                result.distribution.inNeedOfZweitstimme(targetDist))
+            result = result.insert(newvote)
         }
 
         result
@@ -118,11 +144,11 @@ object Generator {
 
     def main(args: Array[String]):Unit = {
         val dist = new Distribution(Map[Candidacy, Double](
-                (c1 -> 1), (c2 -> 0)
+                (c1 -> 0.5), (c2 -> 0.5)
             ), Map[Party, Double](
-                (cdu -> 1), (spd -> 0)
+                (cdu -> 0.5), (spd -> 0.5)
             ))
-        val result = Generator.generate(dist, 2);
+        val result = Generator.generate(dist, 100);
 
         println(result)
         println(result.distribution)
--- u04/wip/schema.sql
+++ u04/wip/schema.sql
@@ -1,30 +1,30 @@
-DROP MATERIALIZED VIEW IF EXISTS ResultsZweitstimme  CASCADE;
-DROP MATERIALIZED VIEW IF EXISTS ResultsErststimme   CASCADE;
-DROP TABLE IF EXISTS ResultsErststimmeManualArchive  CASCADE;
-DROP TABLE IF EXISTS ResultsZweitstimmeManualArchive CASCADE;
-DROP TABLE IF EXISTS CitizenRegistration             CASCADE;
-DROP VIEW  IF EXISTS Stimmzettel                     CASCADE;
-DROP TABLE IF EXISTS StimmzettelData                 CASCADE;
-DROP VIEW  IF EXISTS Wahlschein                      CASCADE;
-DROP TABLE IF EXISTS WahlscheinData                  CASCADE;
-DROP TABLE IF EXISTS Vote                            CASCADE;
-DROP TABLE IF EXISTS Candidacy                       CASCADE;
-DROP TABLE IF EXISTS PartyMembership                 CASCADE;
-DROP TABLE IF EXISTS Landeslistenplatz               CASCADE;
-DROP TABLE IF EXISTS Landesliste                     CASCADE;
-DROP VIEW  IF EXISTS BriefWahlBezirk                 CASCADE;
-DROP TABLE IF EXISTS BriefWahlBezirkData             CASCADE;
-DROP VIEW  IF EXISTS DirektWahlBezirk                CASCADE;
-DROP TABLE IF EXISTS DirektWahlBezirkData            CASCADE;
-DROP TABLE IF EXISTS Wahlbezirk                      CASCADE;
-DROP TABLE IF EXISTS Wahlkreis                       CASCADE;
-DROP TABLE IF EXISTS Party                           CASCADE;
-DROP VIEW  IF EXISTS Candidates                      CASCADE;
-DROP TABLE IF EXISTS CandidatesData                  CASCADE;
-DROP TABLE IF EXISTS hasVoted                        CASCADE;
-DROP TABLE IF EXISTS Citizen                         CASCADE;
-DROP TABLE IF EXISTS ElectionYear                    CASCADE;
-DROP TABLE IF EXISTS FederalState                    CASCADE;
+-- DROP MATERIALIZED VIEW IF EXISTS ResultsZweitstimme  CASCADE;
+-- DROP MATERIALIZED VIEW IF EXISTS ResultsErststimme   CASCADE;
+-- DROP TABLE IF EXISTS ResultsErststimmeManualArchive  CASCADE;
+-- DROP TABLE IF EXISTS ResultsZweitstimmeManualArchive CASCADE;
+-- DROP TABLE IF EXISTS CitizenRegistration             CASCADE;
+-- DROP VIEW  IF EXISTS Stimmzettel                     CASCADE;
+-- DROP TABLE IF EXISTS StimmzettelData                 CASCADE;
+-- DROP VIEW  IF EXISTS Wahlschein                      CASCADE;
+-- DROP TABLE IF EXISTS WahlscheinData                  CASCADE;
+-- DROP TABLE IF EXISTS Vote                            CASCADE;
+-- DROP TABLE IF EXISTS Candidacy                       CASCADE;
+-- DROP TABLE IF EXISTS PartyMembership                 CASCADE;
+-- DROP TABLE IF EXISTS Landeslistenplatz               CASCADE;
+-- DROP TABLE IF EXISTS Landesliste                     CASCADE;
+-- DROP VIEW  IF EXISTS BriefWahlBezirk                 CASCADE;
+-- DROP TABLE IF EXISTS BriefWahlBezirkData             CASCADE;
+-- DROP VIEW  IF EXISTS DirektWahlBezirk                CASCADE;
+-- DROP TABLE IF EXISTS DirektWahlBezirkData            CASCADE;
+-- DROP TABLE IF EXISTS Wahlbezirk                      CASCADE;
+-- DROP TABLE IF EXISTS Wahlkreis                       CASCADE;
+-- DROP TABLE IF EXISTS Party                           CASCADE;
+-- DROP VIEW  IF EXISTS Candidates                      CASCADE;
+-- DROP TABLE IF EXISTS CandidatesData                  CASCADE;
+-- DROP TABLE IF EXISTS hasVoted                        CASCADE;
+-- DROP TABLE IF EXISTS Citizen                         CASCADE;
+-- DROP TABLE IF EXISTS ElectionYear                    CASCADE;
+-- DROP TABLE IF EXISTS FederalState                    CASCADE;
 
 CREATE TABLE IF NOT EXISTS FederalState (
     fsid SERIAL PRIMARY KEY,
@@ -114,13 +114,15 @@ CREATE TABLE IF NOT EXISTS LandesListe (
     llid SERIAL PRIMARY KEY,
     year INT NOT NULL REFERENCES ElectionYear(year) ON DELETE CASCADE,
     pid INT NOT NULL REFERENCES Party(pid) ON DELETE CASCADE,
-    fsid INT NOT NULL REFERENCES FederalState(fsid) ON DELETE CASCADE
+    fsid INT NOT NULL REFERENCES FederalState(fsid) ON DELETE CASCADE,
+    votes INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS Landeslistenplatz (
     llid INT NOT NULL REFERENCES LandesListe(llid) ON DELETE CASCADE,
     idno VARCHAR(32) NOT NULL REFERENCES CandidatesData(idno) ON DELETE CASCADE,
-    position INT UNIQUE NOT NULL,
+    position INT NOT NULL,
+    UNIQUE (llid, position),
     PRIMARY KEY (llid, idno)
 );
 
@@ -136,6 +138,7 @@ CREATE TABLE IF NOT EXISTS Candidacy (
     wkid INT NOT NULL REFERENCES Wahlkreis(wkid) ON DELETE CASCADE,
     idno VARCHAR(255) NOT NULL REFERENCES CandidatesData(idno) ON DELETE CASCADE,
     supportedby INT REFERENCES Party(pid) ON DELETE SET NULL,
+    votes INT NOT NULL DEFAULT 0
     -- EXISTS (SELECT *
     --         FROM PartyMembership
     --         WHERE PartyMembership.idno = Candidacy.idno
@@ -211,45 +214,77 @@ CREATE TABLE IF NOT EXISTS CitizenRegistration (
 
 
 -- Election Results
-CREATE TABLE ResultsErststimmeManualArchive (
-    year INT NOT NULL,
-    wkid INT NOT NULL,
-    idno VARCHAR(32) NOT NULL,
-    votes INT NOT NULL
+
+CREATE FUNCTION incLandeliste(zweitstimme INT) AS (
+    UPDATE TABLE Landesliste
+    SET votes = votes + 1
+    WHERE Landesliste.llid = zweitstimme
 );
 
-CREATE TABLE ResultsZweitstimmeManualArchive (
-    year INT NOT NULL,
-    fsid INT NOT NULL,
-    pid INT NOT NULL,
-    votes INT NOT NULL
+CREATE FUNCTION decLandeliste(zweitstimme INT) AS (
+    UPDATE TABLE Landesliste
+    SET votes = votes - 1
+    WHERE Landesliste.llid = zweitstimme
 );
 
-CREATE MATERIALIZED VIEW ResultsErststimme AS (
-    SELECT wk.year      AS year,
-           wk.wkid      AS wkid,
-           c.idno       AS idno,
-           COUNT(v.vid) AS votes
-    FROM Vote v JOIN
-         Candidacy c ON v.erststimme = c.cid JOIN
-         Wahlkreis wk ON c.wkid = wk.wkid
-    GROUP BY wk.year, wk.wkid, c.idno
-
-    UNION
-
-    SELECT * FROM ResultsErststimmeManualArchive
+CREATE FUNCTION incCandidacy(erststimme INT) AS (
+    UPDATE TABLE Candidacy
+    SET votes = votes + 1
+    WHERE Candidacy.cid = erststimme
 );
 
-CREATE MATERIALIZED VIEW ResultsZweitstimme AS (
-    SELECT l.year       AS year,
-           l.fsid       AS fsid,
-           l.pid        AS pid,
-           COUNT(v.vid) AS votes
-    FROM Vote v JOIN Landesliste l ON v.zweitstimme = l.llid
-    GROUP BY l.year, l.fsid, l.pid
-
-    UNION
-
-    SELECT * FROM ResultsZweitstimmeManualArchive
+CREATE FUNCTION decCandidacy(zweitstimme INT) AS (
+    UPDATE TABLE Candidacy
+    SET votes = votes - 1
+    WHERE Candidacy.cid = zweitstimme;
 );
 
+CREATE FUNCTION shiftVoteCandidacy(from INT, to INT) AS (
+    decCandidacy(from);
+    incCandidacy(to);
+);
+
+CREATE FUNCTION shiftVoteLandeliste(from INT, to INT) AS (
+    decLandeliste(from);
+    incLandeliste(to);
+);
+
+CREATE TRIGGER insertVoteToLandesliste
+AFTER INSERT ON Vote
+EXECUTE PROCEDURE incLandeliste(NEW.zweitstimme);
+
+CREATE TRIGGER updateVoteToLandesliste
+AFTER UPDATE ON Vote
+EXECUTE PROCEDURE shiftVoteLandeliste(OLD.zweitstimme, NEW.zweitstimme)
+
+CREATE TRIGGER insertVoteToCandidacy
+AFTER INSERT ON Vote
+EXECUTE PROCEDURE incCandidacy(NEW.erststimme);
+
+CREATE TRIGGER updateVoteToCandidacy
+AFTER UPDATE ON Vote
+EXECUTE PROCEDURE shiftVoteCandidacy(OLD.zweitstimme, NEW.zweitstimme)
+
+CREATE VIEW AccumulatedErststimmen AS (
+    WITH Totalvotes(wkid, total) AS (
+        SELECT year, wkid, SUM(votes) AS total
+        FROM Candidacy
+        GROUP BY year, wkid
+    )
+    SELECT year, wkid, idno, votes, (CAST(votes AS NUMERIC(8,7)) / total) AS percentage
+    FROM Candidacy NATURAL JOIN Totalvotes
+);
+
+CREATE VIEW AccumulatedZweistimmen AS (
+
+);
+
+
+CREATE TABLE ZweitstimmenArchive (
+    year INT NOT NULL REFERENCES ElectionYear(year),
+    fsid INT NOT NULL REFERENCES FederalState(fsid),
+    pid INT NOT NULL REFERENCES Party(pid),
+    votes INT NOT NULL,
+    CHECK (votes > 0),
+    PRIMARY KEY (year, fsid, pid)
+);
