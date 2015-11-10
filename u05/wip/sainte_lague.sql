79fdca3507c43de9b9204c04c2c6b51c1b050263
Thomas Weber <weber@openinnovator.net>
2015-11-10 19:23:17 +0100

    Fixed product/quotient mistake; recursion of factors stops earlier now.

--- u05/wip/sainte_lague.sql
+++ u05/wip/sainte_lague.sql
@@ -1,15 +1,8 @@
 WITH RECURSIVE
-    Factors(f) AS (
-        VALUES (0.5)
-        UNION
-        SELECT f + 1
-        FROM Factors
-        WHERE f < 299
-    ),
     AggegratedVotesZS(pid, votes, year) AS (
     VALUES
         (1, 100, 2013),
-        (2, 100, 2013),
+        (2, 150, 2013),
         (3, 10, 2013)
     ),
     PartiesToConsider(pid, votes) AS (
@@ -17,11 +10,19 @@ WITH RECURSIVE
         FROM AggegratedVotesZS a1 JOIN AggegratedVotesZS a2 ON a1.year = a2.year
         GROUP BY a1.pid, a1.votes
         HAVING a1.votes > SUM(a2.votes) * 0.05
+    ),
+    Factors(f) AS (
+        VALUES (0.5)
+        UNION
+        SELECT f + 1
+        FROM Factors
+        WHERE f < (300 * (SELECT 1.000 * MAX(votes) / SUM(votes) FROM PartiesToConsider))
     )
 
-SELECT pid, COUNT(product)
-FROM (SELECT az.pid, az.votes * f.f AS product
+SELECT pid, COUNT(quotient)
+FROM (
+    SELECT az.pid, az.votes / f.f AS quotient
       FROM PartiesToConsider az, Factors f
-      ORDER BY product DESC
+      ORDER BY quotient DESC
       LIMIT 299) r
 GROUP BY pid;
