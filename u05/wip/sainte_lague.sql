02f2c307c2948ba29ac1947ad4cdf49cbf35fb3a
Thomas Weber <weber@openinnovator.net>
2015-11-10 19:12:03 +0100

    Added handling of 5% limit to sainte_lague

--- u05/wip/sainte_lague.sql
+++ u05/wip/sainte_lague.sql
@@ -6,16 +6,22 @@ WITH RECURSIVE
         FROM Factors
         WHERE f < 299
     ),
-    AggegratedVotesZS(pid, votes) AS (
+    AggegratedVotesZS(pid, votes, year) AS (
     VALUES
-        (1, 100),
-        (2, 100),
-        (3, 100)
+        (1, 100, 2013),
+        (2, 100, 2013),
+        (3, 10, 2013)
+    ),
+    PartiesToConsider(pid, votes) AS (
+        SELECT a1.pid, a1.votes
+        FROM AggegratedVotesZS a1 JOIN AggegratedVotesZS a2 ON a1.year = a2.year
+        GROUP BY a1.pid, a1.votes
+        HAVING a1.votes > SUM(a2.votes) * 0.05
     )
 
 SELECT pid, COUNT(product)
 FROM (SELECT az.pid, az.votes * f.f AS product
-      FROM AggegratedVotesZS az, Factors f
+      FROM PartiesToConsider az, Factors f
       ORDER BY product DESC
       LIMIT 299) r
 GROUP BY pid;
