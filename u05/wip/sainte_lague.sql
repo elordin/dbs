94d2178c9fc6c47c02040ecbc24500677ce84a57
Thomas Weber <weber@openinnovator.net>
2015-11-12 00:18:57 +0100

    Some attempts to solve Sainte Lague in SQL only.

--- u05/wip/sainte_lague.sql
+++ u05/wip/sainte_lague.sql
@@ -21,52 +21,66 @@ PartiesBeyondFivePercent(pid, votes) AS (
                 WHERE supportedby = a1.pid)
 ),
 
--- Required for Sainte Lague Calculation
--- RECURSIVE
-Factors(f) AS (
-    VALUES (0.5)
+
+WITH RECUSIVE Sitzverteilung(totalseats, ausgleichmissing, pid, mandate) AS (
+    WITH RECURSIVE
+        -- Required for Sainte Lague Calculation
+        -- RECURSIVE
+        Factors(f) AS (
+            VALUES (0.5)
+            UNION ALL
+            SELECT f + 1
+            FROM Factors
+            WHERE f < (totalseats * (SELECT 1.000 * MAX(votes) / SUM(votes)
+                                     FROM PartiesBeyondFivePercent))
+        ),
+
+        -- Seat Distribution for 598 seats using Sainte Lague
+        RegularDistribution AS (
+            SELECT 598, pid, COUNT(quotient) AS mandate
+            FROM (
+                SELECT az.pid, az.votes / f.f AS quotient
+                  FROM PartiesBeyondFivePercent az, Factors f
+                  ORDER BY quotient DESC
+                  LIMIT 598) r
+            GROUP BY pid;
+        ),
+
+        -- Überhangsmandate
+        Ueberhangsmandate AS (
+            SELECT pid, wk.mandate - rd.mandate AS uhms
+            FROM RegularDistribution rd INNER JOIN (
+                    SELECT supportedby AS pid, COUNT(wkid) AS mandate
+                    FROM WahlkreisSieger
+                    WHERE supportedby IS NOT NULL
+                    GROUP BY supportedby
+                ) wk ON rd.pid = wk.pid
+            WHERE rd.mandate < wk.mandate
+        ),
+
+    MandateMitUerberhang AS (
+        SELECT  SUM(rd.mandate) + SUM(um.uhms)
+
+                rd.pid, rd.mandate + COALESCE(uhms, 0) AS Mandate
+        FROM RegularDistribution rd LEFT OUTER JOIN Ueberhangsmandate um
+            ON rd.pid = um.pid
+    ),
+
+        AusgleichRequired AS (
+            SELECT 0 = ALL(SELECT uhms FROM Ueberhangsmandate)
+            -- SELECT 0 < ANY(SELECT rd.mandate = mmu.mandate
+            --                FROM RegularDistribution rd INNER JOIN MandateMitUerberhang mmu
+            --                     ON rd.pid = mmu.pid)
+        ),
+
     UNION ALL
-    SELECT f + 1
-    FROM Factors
-    WHERE f < (@seatcount * (SELECT 1.000 * MAX(votes) / SUM(votes)
-                             FROM PartiesBeyondFivePercent))
-),
+        SELECT *
+        FROM Sitzverteilung sv
+        WHERE sv.ausgleichmissing > 0
+SELECT pid, mandate
+FROM Sitzverteilung
+WHERE ausgleichmissing = 0
 
--- Seat Distribution for 598 seats using Sainte Lague
-RegularDistribution AS (
-    SELECT pid, COUNT(quotient) AS mandate
-    FROM (
-        SELECT az.pid, az.votes / f.f AS quotient
-          FROM PartiesBeyondFivePercent az, Factors f
-          ORDER BY quotient DESC
-          LIMIT 598) r
-    GROUP BY pid;
-),
-
--- Überhangsmandate
-Ueberhangsmandate AS (
-    SELECT pid, wk.mandate - rd.mandate AS uhms
-    FROM RegularDistribution rd INNER JOIN (
-            SELECT supportedby AS pid, COUNT(wkid) AS mandate
-            FROM WahlkreisSieger
-            WHERE supportedby IS NOT NULL
-            GROUP BY supportedby
-        ) wk ON rd.pid = wk.pid
-    WHERE rd.mandate < wk.mandate
-),
-
-MandateMitUerberhang AS (
-    SELECT rd.pid, rd.mandate + COALESCE(uhms, 0) AS Mandate
-    FROM RegularDistribution rd LEFT OUTER JOIN Ueberhangsmandate um
-        ON rd.pid = um.pid
-),
-
-AusgleichRequired AS (
-    SELECT 0 = ALL(SELECT uhms FROM Ueberhangsmandate)
-    -- SELECT 0 < ANY(SELECT rd.mandate = mmu.mandate
-    --                FROM RegularDistribution rd INNER JOIN MandateMitUerberhang mmu
-    --                     ON rd.pid = mmu.pid)
-),
 
 WHILE AusgleichRequired
     SITZZAHL++
@@ -83,3 +97,4 @@ WHILE AusgleichRequired
 -- UNION
 --   SELECT idno, supportedby AS pid
 --   FROM WahlkreisSieger
+)
