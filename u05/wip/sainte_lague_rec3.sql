25118114808b5667a3e00cba25bc16376194dc8b
Thomas Weber <weber@openinnovator.net>
2015-11-12 15:49:53 +0100

    SL #3

---
+++ u05/wip/sainte_lague_rec3.sql
@@ -0,0 +1,80 @@
+WITH RECURSIVE
+Factors(f) AS (
+    VALUES (0.5)
+    UNION ALL
+    SELECT f + 1
+    FROM Factors
+    WHERE f < (598 * (SELECT 1.250 * MAX(citizencount) / SUM(citizencount)
+                             FROM FederalState))
+),
+
+WahlkreisSieger AS (
+    SELECT wk.wkid, c1.idno, c1.supportedby
+    FROM Wahlkreis wk NATURAL JOIN Candidacy c1 INNER JOIN Candidacy c2 ON c1.wkid = c2.wkid
+    WHERE wk.year = 2009
+    GROUP BY wk.wkid, c1.idno, c1.supportedby, c1.votes
+    HAVING c1.votes = MAX(c2.votes)
+    LIMIT 1
+),
+
+AggregatedVotesZS(pid, year, votes) AS (
+    SELECT pid, year, SUM(votes)
+    FROM AccumulatedZweitstimmenWK NATURAL JOIN LandesListe
+    GROUP BY pid, year
+),
+
+SeatsPerFederalState(fsid, seats) AS (
+    SELECT fsid, COUNT(quotient) AS seats
+    FROM (
+        SELECT fs.fsid, fs.citizencount / f.f AS quotient
+          -- TODO: Create FederlStateWithCitizenCount
+          FROM FederalState fs, Factors f
+          ORDER BY quotient DESC
+          LIMIT 598) AS r
+    GROUP BY fsid
+),
+
+-- Parties that managed to have more than 5% or more than 3 Direktmandate or are Minority Parties
+PartiesBeyondFivePercent(pid, votes) AS (
+    SELECT a1.pid, a1.votes
+    FROM Party p NATURAL JOIN AggregatedVotesZS a1 JOIN AggregatedVotesZS a2 ON a1.year = a2.year
+    WHERE a1.year = 2009
+    GROUP BY a1.pid, a1.votes, p.isminority
+    HAVING a1.votes > 0
+       AND p.isminority -- minority parties are exempted from 5% threshold
+        OR a1.votes > SUM(a2.votes) * 0.05
+        OR 2 < (SELECT COUNT(DISTINCT wkid)
+                FROM WahlkreisSieger
+                WHERE supportedby = a1.pid)
+),
+
+SeatsPerLandelisteByZweitstimme(llid, seats) AS (
+    SELECT ll.llid, (
+        WITH RECURSIVE Factors(f) AS (
+            VALUES (0.5)
+            UNION ALL
+            SELECT f + 1
+            FROM Factors
+            WHERE f < (spfs.seats * (SELECT 1.250 * MAX(citizencount) / SUM(citizencount)
+            FROM FederalState))
+        )
+        SELECT COUNT(quotient) AS seats
+        FROM (
+            SELECT azfs.llid, azfs.votes / f.f AS quotient
+            FROM AccumulatedZweitstimmenFS azfs, Factors f
+            ORDER BY quotient
+            LIMIT spfs.seats
+        ) AS r
+        GROUP BY llid
+        HAVING llid = ll.llid
+    ) AS seats
+    FROM PartiesBeyondFivePercent ps -- pid votes
+        NATURAL JOIN LandesListe ll
+        NATURAL JOIN SeatsPerFederalState spfs
+        INNER JOIN AccumulatedZweitstimmenFS azfs ON azfs.llid = ll.llid
+    WHERE ll.year = 2009
+)
+/*
+
+SELECT * FROM SeatsPerLandelisteByZweitstimme
+*/
