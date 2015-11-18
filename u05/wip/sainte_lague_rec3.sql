ee5164cb08d09fc7212a6ddedd13d4f375074c49
gernot <mail@gernotbrunner.at>
2015-11-18 18:09:02 +0100

    -refactored sainte_lague_rec3.sql -added update accumulatedzweitstimmenFS.sql to accumulated zweistimmen from WKs to FSs

--- u05/wip/sainte_lague_rec3.sql
+++ u05/wip/sainte_lague_rec3.sql
@@ -1,79 +1,93 @@
 ﻿
 ---------------- STEP 0: preliminary work ----------------
 /* aggregates Zweitstimmen per Landesliste*/ -- TODO year notwendig?!
-WITH AggregatedZweitstimmenForLL(llid, votes) AS (
-	SELECT llid, SUM(votes)
-	FROM AccumulatedZweitstimmenWK
-	GROUP BY llid
+WITH AggregatedZweitstimmenForLL(fsid, llid, votes) AS (
+	SELECT  ll.fsid, azwfs.llid,azwfs.votes
+	FROM AccumulatedZweitstimmenFS azwfs
+	JOIN Landesliste ll ON azwfs.llid = ll.llid
+	WHERE ll.year = 2013
+),
+
+-- ranks the Direktkandidaten
+RankedCandidacies(wkid, rank, idno, pid) AS (
+	SELECT  c.wkid, 
+		rank() OVER (PARTITION BY c.wkid ORDER BY c.votes desc) as rank,
+		c.idno, c.supportedby as pid
+	FROM Candidacy c
+	JOIN Wahlkreis wk ON c.wkid = wk.wkid
+	WHERE wk.year= 2013  
 ),
 
 -- selects the winning Direktkandidaten
-
--- old VERSION
--- WahlkreisWinners (wkid, idno, pid) AS (
---     SELECT wk.wkid, c1.idno, c1.supportedby as pid
---     FROM Wahlkreis wk NATURAL JOIN Candidacy c1 INNER JOIN Candidacy c2 ON c1.wkid = c2.wkid
---     WHERE wk.year = 2013
---     GROUP BY wk.wkid, c1.idno, c1.supportedby, c1.votes
---     HAVING c1.votes = MAX(c2.votes)
--- ),
 WahlkreisWinners (wkid, idno, pid) AS (
-    SELECT c1.wkid, c1.idno, c1.supportedby as pid
-    FROM Candidacy c1 
-    INNER JOIN 
-	    (SELECT wkid, MAX(votes) as votes
-	    FROM Candidacy
-	    NATURAL JOIN Wahlkreis wk
-	    WHERE wk.year = 2013
-	    GROUP BY wkid) as c2
-    ON c1.wkid = c2.wkid and c1.votes= c2.votes
+    SELECT wkid, idno, pid
+    FROM RankedCandidacies 
+    WHERE rank = 1
 ),
 
 -- aggregates Zweitstimmen per Party -- TODO year notwendig?!
-AggregatedZweitstimmenForParty(pid, year, votes) AS (
-    SELECT pid, year, SUM(votes)
-    FROM AccumulatedZweitstimmenWK NATURAL JOIN LandesListe
-    GROUP BY pid, year
+AggregatedZweitstimmenForParty(pid, votes) AS (
+    SELECT ll.pid, SUM(azwfll.votes)
+    FROM AggregatedZweitstimmenForLL azwfll
+    JOIN LandesListe ll ON azwfll.llid = ll.llid
+    GROUP BY ll.pid
 ),
 
 -- Parties that managed to have more than 5% or more than 3 Direktmandate or are Minority Parties
-PartiesBeyondFivePercent(pid, votes) AS (
-    SELECT a1.pid, a1.votes
-    FROM Party p NATURAL JOIN AggregatedZweitstimmenForParty a1 JOIN AggregatedZweitstimmenForParty a2 ON a1.year = a2.year
-    WHERE a1.year = 2013
-    GROUP BY a1.pid, a1.votes, p.isminority
-    HAVING a1.votes > 0
-       AND p.isminority -- minority parties are exempted from 5% threshold
-        OR a1.votes > SUM(a2.votes) * 0.05
-        OR 2 < (SELECT COUNT(DISTINCT wkid)
-                FROM WahlkreisWinners
-                WHERE pid = a1.pid)
-),
+-- PartiesQualified(pid, votes) AS (
+--     SELECT a1.pid, a1.votes
+--     FROM Party p NATURAL JOIN AggregatedZweitstimmenForParty a1 JOIN AggregatedZweitstimmenForParty a2
+--     GROUP BY a1.pid, a1.votes, p.isminority
+--     HAVING a1.votes > 0
+--        AND p.isminority -- minority parties are exempted from 5% threshold
+--         OR a1.votes > SUM(a2.votes) * 0.05
+--         OR 2 < (SELECT COUNT(DISTINCT wkid)
+--                 FROM WahlkreisWinners
+--                 WHERE pid = a1.pid)
+-- ),
 
 --bring noch nichts......
-PartiesBeyondFivePercent2(pid, votes) AS (
+PartiesQualified(pid, votes) AS (
     SELECT a1.pid, a1.votes
     FROM Party p NATURAL JOIN AggregatedZweitstimmenForParty a1
-    WHERE a1.year = 2013
-       AND a1.votes > 0
+    WHERE a1.votes > 0
        AND (p.isminority
-	    OR a1.votes >= 0.05 * (SELECT SUM(votes) from AggregatedZweitstimmenForParty  where year = 2013)
-	    OR 2 < (SELECT COUNT(DISTINCT wkid) FROM WahlkreisWinners WHERE pid = a1.pid))
+	    OR 2 < (SELECT COUNT(*) FROM WahlkreisWinners WHERE pid = a1.pid)
+	    OR a1.votes >= 0.05 * (SELECT SUM(votes) from AggregatedZweitstimmenForParty))
+	    
+),
+
+AggregatedZweitstimmenForLLQualified(fsid, llid, votes) AS (
+	SELECT  azwfs.fsid, azwfs.llid, azwfs.votes
+	FROM AggregatedZweitstimmenForLL azwfs
+	JOIN Landesliste ll ON azwfs.llid = ll.llid
+	WHERE ll.pid in (select pid from PartiesQualified)
+),
+
+--calculates the number of Wahklkreis wins per Landesliste
+WahlkreisesiegePerPartyPerFS (pid, fsid, seats) AS (
+	SELECT pid, wk.fsid, count(*)
+	FROM WahlkreisWinners wks
+	JOIN Wahlkreis wk ON wks.wkid = wk.wkid
+	JOIN Federalstate fs ON wk.fsid = fs.fsid
+	GROUP BY pid, wk.fsid
 ),
 
 ---------------- STEP 1: Seats per Federalstate ----------------
 -- creates the seat ranking for the Federalstates according to Sainte-Laguë with Höchstzahlverfahren
 RankedSeatsperFederalState(fsid, seatnumber) as (
-	WITH RECURSIVE Factors(f) AS (
-		VALUES (0.5)
+	WITH RECURSIVE Factors(fsid, f) AS (
+		(SELECT fsid, 0.5 from federalstate)
 		UNION ALL
-		SELECT f + 1
-		FROM Factors
-		WHERE f < (598 * (SELECT 1.250 * MAX(citizencount) / SUM(citizencount) FROM FederalState))
-	)
+		(SELECT f.fsid, f.f + 1
+		 FROM Factors f
+		 WHERE f.f < (598 * ((SELECT citizencount FROM FederalState fs where fs.fsid=f.fsid)*1.00/(SELECT sum(citizencount) FROM FederalState)*1.00) +1)
+		)
+	) 
 
 	SELECT fs.fsid, rank() OVER (ORDER BY fs.citizencount/f.f desc) as seatnumber
-	FROM FederalState fs, Factors f
+	FROM FederalState fs
+	JOIN Factors f ON fs.fsid = f.fsid
 ),
 
 -- calculates the number of seats per Federalstate --> RESULT STEP 1
@@ -85,45 +99,47 @@ SeatsPerFederalState(fsid, seats) AS (
 ),
 
 ---------------- STEP 2: Seats per Landesliste for each Federalstate ----------------
--- creates the seat ranking for the Landeslisten by Zweitstimmen according to Sainte-Laguë with Höchstzahlverfahren
-RankedSeatsPerLandesliste (llid, seatnumber) AS (
-	WITH RECURSIVE Factors(fsid, f) AS (
-	    (select fsid, 0.5 from federalstate)
+-- creates the seat ranking for the Landeslisten by Zweitstimmen according to Sainte-Laguë with Höchstzahlverfahren in Federalstate, in Party and in Landesliste
+RankedSeatsPerLandesliste (llid, seatnumberInFS, seatnumberInParty, seatnumberLL) AS (
+	WITH RECURSIVE Factors(llid, f) AS (
+	    (SELECT llid, 0.5 FROM AggregatedZweitstimmenForLLQualified)
 	    UNION ALL
-	    SELECT fsid, f + 1
-	    FROM Factors f
-	    WHERE f < (select max(seats) from SeatsPerFederalState spfs where spfs.fsid = f.fsid)
+	    (SELECT f.llid, f.f + 1
+	     FROM Factors f
+	     WHERE f.f < (Greatest(
+			   ((select seats * (900.0/599.0) from SeatsPerFederalState spfs where spfs.fsid = (select fsid from Landesliste where llid=f.llid)) -- (800.0/599.0) factor if seats in Bundestag reach max.
+			    *((SELECT votes FROM AggregatedZweitstimmenForLLQualified agzwfll where agzwfll.llid=f.llid)*1.00
+			      /(SELECT sum(votes) FROM AggregatedZweitstimmenForLLQualified agzwfll where agzwfll.fsid = (select fsid from Landesliste ll where ll.llid=f.llid))*1.00
+			      )
+			     +1 --todoGB: check if falid now
+			    ), 	
+			    (select seats from WahlkreisesiegePerPartyPerFS wkspppfs where wkspppfs.fsid = (select fsid from Landesliste ll where ll.llid=f.llid)	and wkspppfs.pid = (select pid from Landesliste ll where ll.llid=f.llid))
+			   )
+			 ) 
+	    )
 	)
-	
-	SELECT ll.llid, rank()  OVER
-		(Partition by ll.fsid Order by azfs.votes/f.f desc) as seatnumber
-	FROM PartiesBeyondFivePercent ps -- pid votes
-	NATURAL JOIN LandesListe ll
-	INNER JOIN AggregatedZweitstimmenForLL azfs ON azfs.llid = ll.llid
-	INNER JOIN Factors f ON f.fsid = ll.fsid
-	WHERE ll.year = 2013
+
+	SELECT ll.llid, 
+		rank()  OVER (Partition by ll.fsid Order by azllq.votes/f.f desc) as seatnumberInFS,		--rank within Federalstate	
+		rank()  OVER (Partition by ll.pid Order by azllq.votes/f.f desc) as seatnumberInParty,		--rank within Party
+		rank()  OVER (Partition by ll.pid, ll.llid Order by azllq.votes/f.f desc) as seatnumberInLL	--rank within Landesliste
+		
+	FROM AggregatedZweitstimmenForLLQualified azllq 
+	JOIN LandesListe ll ON azllq.llid = ll.llid
+	JOIN Factors f ON f.llid = azllq.llid
 ),     
 
 -- calculates the number of seats for each Landesliste according to the seat ranking an the totoal number of available seats --> RESULT STEP 2
 SeatsPerLandelisteByZweitstimme(llid, seats) AS (
-	SELECT llid, Count(*) as numberOfSeats 
-	from RankedSeatsPerLandesliste  
-	NATURAL JOIN LandesListe ll
-	NATURAL JOIN SeatsPerFederalState spfs
-	where seatnumber <= spfs.seats
-	group by llid   
+	SELECT rspll.llid, Count(*) as numberOfSeats 
+	FROM RankedSeatsPerLandesliste rspll
+	JOIN LandesListe ll ON ll.llid = rspll.llid
+	JOIN SeatsPerFederalState spfs ON spfs.fsid = ll.fsid 
+	WHERE seatnumberInFS <= spfs.seats
+	GROUP BY rspll.llid   
 ),
 
 ---------------- STEP ZWISCHENERGEBNIS: minimum Number of Seats per Party ----------------
---calculates the number of Wahklkreis wins per Landesliste
-WahlkreisesiegePerPartyPerFS (pid, fsid, seats) AS (
-	select pid, wk.fsid, count(*)
-	from WahlkreisWinners wks
-	join Wahlkreis wk ON wks.wkid = wk.wkid
-	join Federalstate fs ON wk.fsid = fs.fsid
-	group by pid, wk.fsid
-),
-
 --calculates the minimum number of seats per Party in each Federalstate, based on Zweistimmen and won Wahlkreise
 MinimumSeatsPerPartyPerFederalstate (pid, fsid, minSeats) AS (
 	select ll.pid, ll.fsid as fsid, GREATEST(coalesce(wkspppfs.seats,0), coalesce(spllbzs.seats,0)) as minSeats
@@ -142,17 +158,19 @@ MinimumSeatsPerParty (pid,minSeats) AS (
 ---------------- STEP 3: final number of Seats per Party ----------------
 -- creates the seat ranking for the Bundestag for each Party based on Zweitstimmen according to Sainte-Laguë with Höchstzahlverfahren
 RankedSeatsPerParty(pid, seatnumberTotal, seatnumberParty) AS (
-	WITH RECURSIVE Factors(f) AS (
-	    VALUES (0.5)
+	WITH RECURSIVE Factors(pid, f) AS (
+	    (SELECT pid, 0.5 from PartiesQualified)
 	    UNION ALL
-	    SELECT f + 1
-	    FROM Factors f
-	    WHERE f < (400)
+	    (SELECT f.pid, f + 1
+	     FROM Factors f
+	     WHERE f < (900 * (SELECT votes FROM PartiesQualified p where p.pid = f.pid)*1.00 / (SELECT sum(votes) FROM PartiesQualified)*1.00 +1) --900 = max. seats in Bundestag
+	    )
 	)
 
-	SELECT pbfp.pid, rank()  OVER (Order by pbfp.votes/f.f desc) as seatnumberTotal,	--rank overall seats in Bundestag
-	rank()  OVER (Partition by pbfp.pid Order by pbfp.votes/f.f desc) as seatnumberParty	--rank within the Party
-	FROM PartiesBeyondFivePercent pbfp , Factors f
+	SELECT p.pid, rank()  OVER (Order by p.votes/f.f desc) as seatnumberTotal,	--rank overall seats in Bundestag
+	rank()  OVER (Partition by p.pid Order by p.votes/f.f desc) as seatnumberParty	--rank within the Party
+	FROM PartiesQualified p
+	JOIN Factors f ON p.pid = f.pid
 ),
 
 --calculates the final number of seats for Bundestag
@@ -172,31 +190,11 @@ TotalNumberOfSeatsPerParty(pid,seats) AS (
 ),
 
 ---------------- STEP 4: final number of Seats per Landesliste ----------------
--- creates the seat ranking for each Landesliste and within its Party based on Zweitstimmen
-RankedSeatsPerLandesliste2 (llid, seatnumberInParty, seatnumberLL) AS (
-	WITH RECURSIVE Factors(fsid, f) AS (
-	    (select fsid, 0.5 from federalstate)
-	    UNION ALL
-	    SELECT fsid, f + 1
-	    FROM Factors f
-	    WHERE f < (select max(seats)+20 from SeatsPerFederalState spfs where spfs.fsid = f.fsid)
-	)
-
-	SELECT ll.llid,
-		rank()  OVER (Partition by p.pid Order by azfs.votes/f.f desc) as seatnumberInParty,		--rank within Party
-		rank()  OVER (Partition by p.pid, ll.llid Order by azfs.votes/f.f desc) as seatnumberInLL	--rank within Landesliste
-	FROM PartiesBeyondFivePercent p -- pid votes
-	NATURAL JOIN LandesListe ll
-	INNER JOIN AggregatedZweitstimmenForLL azfs ON azfs.llid = ll.llid
-	INNER JOIN Factors f ON f.fsid = ll.fsid
-	WHERE ll.year = 2013
-),
-
 -- removes the seats used by Direktkandidaturen and re-ranks the remaining seats by the number within the Party
 RemainingRankedSeatsPerLandesliste (llid, seatnumberInParty) AS (
 	SELECT rspll.llid,
 	rank()  OVER (Partition by ll.pid  Order by rspll.seatnumberInParty asc) as seatnumberInParty		--rerank the remaining seats
-	FROM RankedSeatsPerLandesliste2 rspll
+	FROM RankedSeatsPerLandesliste rspll
 	JOIN Landesliste ll ON ll.llid=rspll.llid
 	Left Outer JOIN WahlkreisesiegePerPartyPerFS wkspll ON wkspll.pid=ll.pid and wkspll.fsid=ll.fsid
 	where rspll.seatnumberLL > coalesce(wkspll.seats,0)							--remove seats won by Direktkandidaturen
@@ -223,10 +221,11 @@ AdditionalSeatsToDirektmandatePerLandeliste(llid, seats) AS (
 -- calculates the number of seats per Landesliste by adding the Direktkandidaturen plus the raimining seats, filled by Landeslistenplätzen --> RESULT STEP 4
 SeatsPerLandesliste (llid, seats)  AS(
 	select ll.llid, (coalesce(wkspll.seats,0)+coalesce(astdkpll.seats,0)) as seats
-	FROM Landesliste ll 
+	FROM AggregatedZweitstimmenForLLQualified azllq
+	JOIN Landesliste ll ON azllq.llid=ll.llid
 	LEFT OUTER JOIN WahlkreisesiegePerPartyPerFS wkspll ON ll.pid = wkspll.pid AND ll.fsid = wkspll.fsid
 	LEFT OUTER JOIN AdditionalSeatsToDirektmandatePerLandeliste astdkpll ON ll.llid = astdkpll.llid
-	WHERE ll.year = 2013 AND ll.pid in (select pid from PartiesBeyondFivePercent) AND (coalesce(wkspll.seats,0)+coalesce(astdkpll.seats,0)) >0
+	WHERE (coalesce(wkspll.seats,0)+coalesce(astdkpll.seats,0)) >0
 ),
 
 ---------------- STEP 5: List of Delegates for Bundestag ----------------
@@ -237,11 +236,11 @@ SeatsPerLandesliste (llid, seats)  AS(
 	 NATURAL JOIN Wahlkreis wk)
 	UNION
 	(select c.idno, ll.fsid, ll.pid, NULL as wkid,llp.position as rank
-	 from Candidates c
-	 NATURAL JOIN Landeslistenplatz llp
-	 NATURAL JOIN Landesliste ll
-	 WHERE ll.year = 2013 AND ll.pid in (select pid from PartiesBeyondFivePercent) 
-	       AND  not exists (select * from WahlkreisWinners wks where wks.idno = c.idno)	-- remove Landeslistenplätze which won a Direktmandat
+	 from AggregatedZweitstimmenForLLQualified azllq
+	 JOIN Landesliste ll ON ll.llid = azllq.llid
+	 JOIN Landeslistenplatz llp ON llp.llid = ll.llid
+	 JOIN Candidates c ON c.idno = llp.idno 
+	 WHERE not exists (select * from WahlkreisWinners wks where wks.idno = c.idno)	-- remove Landeslistenplätze which won a Direktmandat
 	 )
  ),
 
@@ -264,31 +263,31 @@ Delegates (idno, fsid, pid, wkid, rankInFS) AS (
 ------------------------------ TESTING ------------------------------
 ---------- STEP 1: Seats per Federalstate ----------
 ----Ranked Seats
--- select fs.name, rspfs.seatnumber
+-- SELECT fs.name, rspfs.seatnumber
 -- FROM RankedSeatsperFederalState rspfs
--- NATURAL JOIN FederalState fs
+-- JOIN FederalState fs ON rspfs.fsid = fs.fsid
 -- ORDER BY rspfs.seatnumber
 
 ----Seats per fs
 -- SELECT fs.name, spfs.seats
--- from SeatsPerFederalstate spfs
--- NATURAL JOIN FederalState fs
+-- FROM SeatsPerFederalstate spfs
+-- JOIN FederalState fs ON spfs.fsid = fs.fsid
 -- ORDER BY fs.fsid
 
 ---------------- STEP 2: Seats per Landesliste for each Federalstate ----------------
 ----Ranked seats
--- SELECT rspll.seatnumber, fs.name, p.name
+-- SELECT rspll.seatnumberInFS, fs.name, p.name
 -- FROM RankedSeatsPerLandesliste rspll
--- NATURAL JOIN Landesliste ll
--- NATURAL JOIN Federalstate fs
+-- JOIN Landesliste ll ON ll.llid = rspll.llid
+-- JOIN Federalstate fs ON fs.fsid = ll.fsid
 -- JOIN Party p on ll.pid = p.pid
 -- WHERE fs.name='Thüringen'
 
 ----Number of Seats per Landesliste
 -- SELECT spllpzw.seats, fs.name, p.name
 -- FROM SeatsPerLandelisteByZweitstimme spllpzw
--- NATURAL JOIN Landesliste ll
--- NATURAL JOIN Federalstate fs
+-- JOIN Landesliste ll ON ll.llid = spllpzw.llid
+-- JOIN Federalstate fs ON fs.fsid = ll.fsid
 -- JOIN Party p on ll.pid = p.pid
 -- WHERE fs.name='Thüringen'
 -- ORDER BY spllpzw.seats desc
@@ -297,15 +296,14 @@ Delegates (idno, fsid, pid, wkid, rankInFS) AS (
 ---Minimum seats per Party per Federalstate
 -- SELECT mspppf.minSeats, fs.name, p.name
 -- FROM MinimumSeatsPerPartyPerFederalstate mspppf
--- NATURAL JOIN Landesliste ll
--- NATURAL JOIN Federalstate fs
--- JOIN Party p on ll.pid = p.pid
+-- JOIN Federalstate fs ON mspppf.fsid = fs.fsid
+-- JOIN Party p on mspppf.pid = p.pid
 -- WHERE fs.name='Thüringen'
 
 ----Minimum seats per Party in Bundestag
 -- SELECT mspp.minSeats, p.name
 -- FROM MinimumSeatsPerParty mspp
--- NATURAL JOIN Party p 
+-- JOIN Party p ON mspp.pid = p.pid
 
 
 ---------------- STEP 3: final number of Seats per Party ----------------
@@ -313,42 +311,44 @@ Delegates (idno, fsid, pid, wkid, rankInFS) AS (
 ----ranked Seats in Bundestag, total and in Party
 -- SELECT p.name, rspp.seatnumberTotal, rspp.seatnumberParty
 -- FROM RankedSeatsPerParty rspp
--- NATURAL JOIN Party p 
+-- JOIN Party p ON rspp.pid = p.pid
 -- ORDER BY rspp.seatnumberTotal
 
 ----total number of seats in Bundestag for Party
 -- SELECT p.name, tnospp.seats
 -- FROM TotalNumberOfSeatsPerParty tnospp
--- NATURAL JOIN Party p 
+-- JOIN Party p ON tnospp.pid = p.pid 
 -- ORDER BY tnospp.seats desc
 
 
 ---------------- STEP 4: final number of Seats per Landesliste ----------------
 ----ranks seats fro each Landesliste within Party and LL
 -- SELECT fs.name, p.name, rspll.seatnumberInParty, rspll.seatnumberLL
--- FROM RankedSeatsPerLandesliste2 rspll
--- NATURAL JOIN Landesliste ll
--- NATURAL JOIN Federalstate fs
+-- FROM RankedSeatsPerLandesliste rspll
+-- JOIN Landesliste ll ON ll.llid = rspll.llid
+-- JOIN Federalstate fs ON ll.fsid = fs.fsid
 -- JOIN Party p on ll.pid = p.pid
 -- WHERE fs.name='Thüringen' AND p.name='CDU'
 
 ----total number of seats per Landesliste
 -- SELECT fs.name, p.name, spll.seats
 -- FROM SeatsPerLandesliste spll
--- NATURAL JOIN Landesliste ll
--- NATURAL JOIN Federalstate fs
+-- JOIN Landesliste ll ON ll.llid = spll.llid
+-- JOIN Federalstate fs ON ll.fsid = fs.fsid
 -- JOIN Party p on ll.pid = p.pid
 -- WHERE p.name='CDU'
 
 ---------------- STEP 5: List of Delegates for Bundestag ----------------
 SELECT  c.Lastname, c.Firstname, p.name, fs.name, d.rankInFS 
 FROM Delegates d 
-NATURAL JOIN Candidates c
-NATURAL JOIN Federalstate fs
+JOIN Candidates c ON d.idno = c.idno
+JOIN Federalstate fs ON fs.fsid = d.fsid
 JOIN Party p ON p.pid=d.pid
 WHERE p.name='CDU'
 ORDER BY c.Lastname, c.Firstname
 
+-- SELECT  Count(*)
+-- FROM Delegates d 
 
 
 
